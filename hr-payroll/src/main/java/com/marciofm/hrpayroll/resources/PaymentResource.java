9117c3d674fd4b73f9d66edd41d5fc74386b45cc
Marcio Franco <marciofmarcelino@gmail.com>
2024-07-04 22:55:47 -0300

    resilience4j fault tolerance

--- hr-payroll/src/main/java/com/marciofm/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/marciofm/hrpayroll/resources/PaymentResource.java
@@ -10,17 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.marciofm.hrpayroll.entities.Payment;
 import com.marciofm.hrpayroll.services.PaymentService;
 
+import feign.FeignException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
+	
+	private static final String PAYMENTS_SERVICE = "payments";
 	
 	@Autowired
 	private PaymentService service;
 	
 	@GetMapping(value = "/{workerId}/days/{days}")
+	@CircuitBreaker(name = PAYMENTS_SERVICE, fallbackMethod = "getPaymentAlternative")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok().body(payment);
 	}
 	
+	public ResponseEntity<Payment> getPaymentAlternative( Long workerId, Integer days, FeignException e){
+		Payment payment = new Payment("Bob", 200.0,20);
+		return ResponseEntity.ok().body(payment);
+	}
 }
--- hr-payroll/src/main/java/com/marciofm/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/marciofm/hrpayroll/services/PaymentService.java
@@ -1,8 +1,5 @@
 package com.marciofm.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
